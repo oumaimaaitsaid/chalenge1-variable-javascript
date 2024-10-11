51a52f34863075b42027e9705f704d115a272e0a
oumaima ait said <oumaimabellanova@gmail.com>
2024-10-11 16:54:15 +0100

    fin

--- exercice.js
+++ exercice.js
@@ -18,7 +18,6 @@ let major;
 if(age>=18)
     {
         major=true;
-
 }
 else{
     major= false
@@ -35,20 +34,20 @@ console.log('y='+y);
 //Exercice 6 : Calcul de la circonférence d'un cercle
 let rayon = 15;
 let C = 2 * Math.PI * rayon;
-console.log( "la circonférence d'un cercle est"+C);
+console.log( "la circonference d'un cercle est"+C.toFixed(2));
 //Exercice 7 : Conversion de température
 let celsius =25;
 let F = celsius * 9/5 + 32;
-console.log( "Conversion de température est"+ F);
+console.log( "Conversion de temperature est"+ F);
 //Exercice 8 : Calcul de l'âge
 let anneeNaissance =1996;
 let anneeActuelle =2024;
-console.log("age est "+ anneeActuelle -anneeNaissance );
-//Exercice 9 : Calcul du périmètre d'un rectangle
+console.log( anneeActuelle -anneeNaissance );
+//Exercice 9 : Calcul du perimètre d'un rectangle
 let longueur =10; 
 let largeur =20;
 P =2 * (longueur + largeur)
-console.log("Le périmètre du rectangle est : " +P);
+console.log("Le perimetre du rectangle est : " +P);
 //Exercice 10 : Moyenne de trois nombres
 let note1= 15;
 let note2 =12.5;
